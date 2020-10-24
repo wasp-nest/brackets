49f49762d75679056ccbd98d3bf74af81155b9cc
Elena Zimakova <Elena_Zimakova@epam.com>
2020-10-25 00:17:01 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const parenthesisArr = [];
+    const leftBrackets = [];
+    const rightBrackets = [];
+
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    for (let i = 0; i < bracketsConfig.length; ++i) {
+        leftBrackets.push(bracketsConfig[i][0]);
+        rightBrackets.push(bracketsConfig[i][1]);
+
+        const leftCount = str.split('').filter(x => x === bracketsConfig[i][0]).length;
+        const rightCount = str.split('').filter(x => x === bracketsConfig[i][1]).length;
+        if (leftCount !== rightCount) {
+            return false;
+        }
+    }
+
+    for (let i = 0; i < str.length; ++i) {
+        const curBracket = str[i];
+        const curLength = parenthesisArr.length;
+        const lastBracket = parenthesisArr[curLength - 1];
+        const isLeftBracketLast = leftBrackets.indexOf(lastBracket) !== -1;
+
+        if (curLength === 0 && rightBrackets.indexOf(curBracket) !== -1 && (leftBrackets.indexOf(curBracket) !== rightBrackets.indexOf(curBracket))) {
+            return false;
+        } else if (isLeftBracketLast &&
+            (leftBrackets.indexOf(lastBracket) === rightBrackets.indexOf(curBracket))) {
+            parenthesisArr.pop();
+            continue;
+        }
+
+        parenthesisArr.push(curBracket);
+    }
+
+    return parenthesisArr.length === 0;
 }
